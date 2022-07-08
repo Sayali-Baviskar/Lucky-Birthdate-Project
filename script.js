a52c6c6a8b8cec6dbda8f71dbe89ab0dbfbdd27c
Sayali Baviskar <62566100+Sayali-Baviskar@users.noreply.github.com>
2022-07-08 21:24:21 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,59 +1,16 @@
-// const dateOfBirth = document.querySelector("#bdate");
-// const luckyNumber = document.querySelector("#luckyno");
-// const checkButton = document.querySelector("#btnsubmit");
-// const displayMessage = document.querySelector("#output-container");
-
-// const calculateSum = (date) => {
-//   let sum = 0;
-//   date = date.replaceAll("-", "");
-//   for (let digit of date) {
-//     sum = sum + Number(digit);
-//   }
-//   return sum;
-// };
-
-// const checkIsNumberLucky = (sumOfDate, numberToCheck) => {
-//   console.log(sumOfDate, numberToCheck);
-//   if (sumOfDate % numberToCheck === 0) {
-//     return showMessage(`${numberToCheck} is a lucky number!! 🥳 🥳 🥳 `);
-//   }
-//   showMessage(`${numberToCheck} is not that lucky 😕`);
-// };
-
-// const showMessage = (message) => {
-//   displayMessage.innerText = message;
-// };
-
-// checkButton.addEventListener("click", () => {
-//   const date = dateOfBirth.value;
-//   const numberToCheck = luckyNumber.value;
-//   if (date && numberToCheck) {
-//     const sumOfDate = calculateSum(date);
-//     checkIsNumberLucky(sumOfDate, numberToCheck);
-//   } else {
-//     showMessage("Please enter both the fields");
-//   }
-// });
-
-
-
-
-
 const date= document.querySelector("#bdate");
 const luckyno= document.querySelector("#luckyno");
 const btn= document.querySelector("#btnsubmit");
 const displayMessage = document.querySelector("#output-container");
 
-
-
 function compareValues(dob, lno, sum)
 {
     if(sum % lno ==0)
     // console.log("Lucky NO");
-    displayMessage.value="Lucky NO";
+    displayMessage.innerHTML="Yayy! It's a Lucky NO";
     else
     // console.log("Not a Lucky No");
-    displayMessage.value="Not a Lucky NO";
+    displayMessage.innerHTML="Ohh! It's Not a Lucky NO";
 }
 
 function Sumation(dob)
